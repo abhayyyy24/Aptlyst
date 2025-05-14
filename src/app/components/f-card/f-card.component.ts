7be560e6459b92c240842abc2bef0c6668c0865b
abhayyyy24 <pawar2abhay1910@gmail.com>
2025-05-14 06:44:41 +0530

    hero done

---
+++ src/app/components/f-card/f-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'f-card',
+  imports: [],
+  templateUrl: './f-card.component.html',
+  styleUrl: './f-card.component.css'
+})
+export class FCardComponent {
+
+}
