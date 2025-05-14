43fecabd9d647b2ebad5241ea9eab4e8c56b98b4
abhayyyy24 <pawar2abhay1910@gmail.com>
2025-05-14 15:11:27 +0530

    signup done

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'signup',
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
